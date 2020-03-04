0f27cc79d9a49d600f49503a20f03493854ae223
John Dodd <johndodd90@gmail.com>
2020-03-04 02:18:33 +0000

    play again feature added

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Game values
 let min = 1,
   max = 10,
-  winningNum = 2,
+  winningNum = getRandomNum(min, max),
   guessesLeft = 3;
 
 // UI Elements
@@ -15,6 +15,13 @@ const game = document.querySelector("#game"),
 // Assign UI min and max
 minNum.textContent = min;
 maxNum.textContent = max;
+
+//Play again event listener
+game.addEventListener("mousedown", function(e) {
+  if (e.target.className === "play-again") {
+    window.location.reload();
+  }
+});
 
 // Listen for Guess
 guessBtn.addEventListener("click", function() {
@@ -62,6 +69,15 @@ function gameOver(won, msg) {
 
   // Set message to say you have won
   setMessage(msg, color);
+
+  // Play Again?
+  guessBtn.value = "Play Again";
+  guessBtn.className += "play-again";
+}
+
+// Get Winning Num
+function getRandomNum() {
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 // Set message
